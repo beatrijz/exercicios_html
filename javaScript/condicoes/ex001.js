e6a9966636e25a2af32ebd57136dfccf63f45a91
Beatriz Moura <b3am0ur4@outlook.com>
2023-05-16 00:44:04 -0300

    Execícios utilizando javaScript, html e css

---
+++ javaScript/condicoes/ex001.js
@@ -0,0 +1,6 @@
+let vel = 60.5
+console.log (`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){ //condição simples
+    console.log ("Você passou da velocidade permitida, foi multado!")
+} 
+    console.log ("Dirija sempre usando o cinto de segunrança!")
